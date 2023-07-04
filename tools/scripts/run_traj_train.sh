0ccbe3d1512c2e14a1e5e854b385928e4252c46a
zxSu <33048211+zxSu@users.noreply.github.com>
2023-07-04 18:05:28 +0800

    Delete tools directory

--- tools/scripts/run_traj_train.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=/home/suzx/eclipse-workspace/crossmodal_traj:/home/suzx/eclipse-workspace/crossmodal_traj/MEBOW/lib:/home/suzx/eclipse-workspace/crossmodal_traj/MEBOW/tools:$PYTHONPATH
-
-echo $PYTHONPATH
-
-python ../my_traj_train.py
